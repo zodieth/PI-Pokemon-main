4d8b1cec2288d496a8e0e7153305857ee12e179e
zodieth <contact.prodzodi@gmail.com>
2023-01-03 20:37:41 -0300

    BASEurl change

--- api/src/app.js
+++ api/src/app.js
@@ -20,8 +20,10 @@ server.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
     "https://pi-pokemon-main-sigma.vercel.app/"
-    // "http://localhost:3000"
-  ); // "https://pi-pokemon-main-3ie6t8jfy-zodieth.vercel.app",
+  );
+  // "https://pi-pokemon-main-production-2ec5.up.railway.app/"
+  // "http://localhost:3000"
+  // "https://pi-pokemon-main-3ie6t8jfy-zodieth.vercel.app",
 
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
